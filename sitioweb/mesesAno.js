e1d0f63d88878cda8b987e1609cc252facd82176
Gustavo Guerrero <gustavo.guerrero@buscojobs.com>
2022-04-05 08:16:12 -0300

    first commit

---
+++ sitioweb/mesesAno.js
@@ -0,0 +1,7 @@
+
+
+const mesesDelAno = [ "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Setiembre", "Octubre", "Noviembre", "Diciembre"];
+
+for (mes in mesesDelAno) {
+    console.log(mes)
+}
